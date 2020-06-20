0f94aec5dc525dcdfa99262ac713270274252cf2
codeBlooded1997 <52186148+codeBlooded1997@users.noreply.github.com>
2020-06-20 08:48:45 -0400

    updated views.py

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -3,10 +3,23 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from posts.views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-
+    path('', PostListView.as_view(), name='list'),
+    path('<slug>/', PostDetailView.as_view(), name='detail'),
+    path('<slug>/update', PostUpdateView.as_view(), name='update'),
+    path('<slug>/delete', PostDeleteView.as_view(), name='delete'),
+    path('create', PostCreateView.as_view(), name='create'),
 ]
 
 if settings.DEBUG:
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post, PostView, Like, Comment
 
-# Create your views here.
+
+class PostListView(ListView):
+    model = Post
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+
+
+class PostDeleteView(DeleteView):
+    model = Post
